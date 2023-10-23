1ff22349ca678e24b61959374556c5f9fddac4e4
CHANDRAKALA-WAGH <chandrakalawagh14@gmail.com>
2023-10-23 10:27:06 +0530

    first commit

---
+++ newfile.py
@@ -0,0 +1,9 @@
+class Student:
+   def set_data(self, org, code):
+       self.org = org
+       self.code = code
+
+class Sports(Student):
+   def set_name(self, sport, marks ):
+      self.sport = sport
+      self.marks = marks
